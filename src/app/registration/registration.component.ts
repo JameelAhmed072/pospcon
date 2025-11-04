a64425545fd219fb537203db2e21b2b46654d38c
Jameel Ahmed <ahmedjameel072@gmail.com>
2025-11-04 17:49:17 +0500

    Latest changes in organizing commitee and national and international faculty

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -10,4 +10,10 @@ export class RegistrationComponent {
   downloadForm(): void {
     window.open('/assets/Registration-Form-POSPCON-2026.pdf', '_blank');
   }
+
+  openGoogleForm(): void {
+    // TODO: Replace placeholder URL with actual Google Form link
+    const googleFormUrl = 'https://docs.google.com/forms/d/1QBy5muupEW1eP2ZdeGi6oiBMUCf8EaT7lpSzQaRTe5Q/edit';
+    window.open(googleFormUrl, '_blank');
+  }
 }
